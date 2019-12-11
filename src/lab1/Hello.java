68f81dca516bd37f8bd368f91c153afd3a1721da
s361211760016 <40586013+s361211760016@users.noreply.github.com>
2019-12-11 09:08:03 +0700

    Update Hello.java

--- src/lab1/Hello.java
+++ src/lab1/Hello.java
@@ -2,7 +2,7 @@ package lab1;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello Word,i am sudarat");
+        System.out.println("Hello Word,i am sudarat krodsrimai");
         System.out.println("361211760016");
         System.out.println("MIT");
         System.out.println("faculty of Managemt Technology");
