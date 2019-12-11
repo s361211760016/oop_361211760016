f7f8c86eae4c9baa3e7fc4c7aaa2873df7bfc1dc
sudarat <airair1125@gmail.com>
2019-12-11 10:51:00 +0700

    java2

---
+++ src/lab2/basicjava2.java
@@ -0,0 +1,38 @@
+package lab2;
+
+import java.util.Scanner;
+
+public class basicjava2 {
+    public  static void main (String[]args) {
+        //input
+        Scanner sc = new Scanner(System.in);
+
+//        System.out.println("Enter your name");
+//        String name = sc.nextLine();
+//
+//        System.out.println("Your name is "+name);
+//
+//        System.out.print("Enter your email:");
+//        String email = sc.nextLine();
+//        System.out.print("Your email is"+email);
+//
+//        System.out.print("Enter your old:");
+//        String old = sc.nextLine();
+//        System.out.print("Your email is"+old);
+
+        //input integer and floating
+        System.out.print("Enter an number: ");
+        int num = sc.nextInt();
+        System.out.print("Your entered number: "+num);
+
+        System.out.print("Enter an number: ");
+        double d = sc.nextDouble();
+        System.out.print(d);
+    }
+
+
+
+
+
+
+}
