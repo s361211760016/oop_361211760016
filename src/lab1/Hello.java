47d8f3ae360f43c780cefb1725ec5b56f8a43d18
sudarat <airair1125@gmail.com>
2019-12-11 08:59:41 +0700

    Initial commit

---
+++ src/lab1/Hello.java
@@ -0,0 +1,19 @@
+package lab1;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Word,i am sudarat");
+        System.out.println("MIT");
+
+
+
+    } //main
+
+
+
+
+
+
+
+
+} //class
