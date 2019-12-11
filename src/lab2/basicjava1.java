8fe8a9caf49b3b9ec06638bc33cad13fd0503e56
sudarat <airair1125@gmail.com>
2019-12-11 10:09:36 +0700

    Test

---
+++ src/lab2/basicjava1.java
@@ -0,0 +1,57 @@
+package lab2;
+
+import lab1.Hello;
+
+import javax.print.DocFlavor;
+
+public class basicjava1 {
+
+    public static void main(String[] args) {
+
+        //variables
+        //integer
+        int x;
+        x =10;
+        System.out.println(x);
+        int y = -100;
+        System.out.println(y);
+
+        System.out.println(x+y);
+        //floating
+        float f =10.00f;
+       double d = 100.12345;
+       System.out.println(f);
+       System.out.println(d);
+       double n = f * d;
+       System.out.println(n);
+       //Charector
+        char c = 'h';
+        System.out.println(c);
+        System.out.println(c+ "Hello");
+
+        //Strng
+        String s = "sudarat krodsrimai";
+        System.out.println(s);
+        String msg = "@Saiyai";
+        String t = s+msg;
+        System.out.println(t);
+        System.out.println(t.length());
+
+        System.out.println(s.concat(msg));
+
+        System.out.println(t.toUpperCase());
+
+
+
+
+
+
+
+
+        }
+
+
+    } //class
+
+
+
