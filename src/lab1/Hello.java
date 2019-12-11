79bdedab6543629ae72e75e767b0d701780cfa2c
sudarat <airair1125@gmail.com>
2019-12-11 09:06:36 +0700

    Test

--- src/lab1/Hello.java
+++ src/lab1/Hello.java
@@ -3,7 +3,9 @@ package lab1;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Word,i am sudarat");
+        System.out.println("361211760016");
         System.out.println("MIT");
+        System.out.println("faculty of Managemt Technology");
 
 
 
