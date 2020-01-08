c9fbb83ba40dc0d43353bcafb9ded04be5752c83
sudarat <airair1125@gmail.com>
2020-01-08 10:27:10 +0700

    Lab4:control

--- src/lab3/lab4/Method_Overloading.java
+++ src/lab3/lab4/Method_Overloading.java
@@ -14,6 +14,7 @@ public class Method_Overloading {
     public static int sum(int x, int y){
         return x + y;
 
+
     }
     public static double sum (double x, double y){
         return x+y;
